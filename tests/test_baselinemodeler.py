96e27bdc22bc0cd6a3c164983c754654148ef071
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-10-22 18:14:58 +0900

    BaselineModelerのテストコードを追加

---
+++ tests/test_baselinemodeler.py
@@ -0,0 +1,35 @@
+import argparse
+import logging
+import sys
+import os
+sys.path.append(os.path.abspath("../src/aiolib"))
+
+from modeling.baseline.modeling import BaselineModeler
+
+logging_fmt="%(asctime)s %(levelname)s: %(message)s"
+logging.basicConfig(format=logging_fmt)
+
+def main(
+    train_input_dir:str,
+    dev_input_dir:str,
+    bert_model_dir:str,
+    result_save_dir:str):
+    modeler=BaselineModeler(train_input_dir,dev_input_dir,bert_model_dir)
+    modeler.train_and_eval(result_save_dir=result_save_dir)
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument("--train_input_dir",type=str)
+    parser.add_argument("--dev_input_dir",type=str)
+    parser.add_argument("--bert_model_dir",type=str)
+    parser.add_argument("--result_save_dir",type=str)
+
+    args=parser.parse_args()
+
+    main(
+        args.train_input_dir,
+        args.dev_input_dir,
+        args.bert_model_dir,
+        args.result_save_dir
+    )
