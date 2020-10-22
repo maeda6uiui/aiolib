0008ebc4945c3c847b3dba5c46bd9055ee8a65bb
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-10-22 12:53:57 +0900

    テストコードを更新

--- tests/test_exampleencoder.py
+++ tests/test_exampleencoder.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import sys
 import os
@@ -8,21 +9,48 @@ from util.encode import ExampleEncoder
 logging_fmt="%(asctime)s %(levelname)s: %(message)s"
 logging.basicConfig(format=logging_fmt)
 
-if __name__=="__main__":
-    train_example_filepath="./WorkingDir/Data/train_questions.json"
-    dev1_example_filepath="./WorkingDir/Data/dev1_questions.json"
-    dev2_example_filepath="./WorkingDir/Data/dev2_questions.json"
-    leaderboard_example_filepath="./WorkingDir/Data/aio_leaderboard.json"
-    context_filepath="./WorkingDir/Data/candidate_entities.json.gz"
-    bert_model_dir="USE_DEFAULT"
-
-    train_save_dir="./WorkingDir/EncodedText/Train"
-    dev1_save_dir="./WorkingDir/EncodedText/Dev1"
-    dev2_save_dir="./WorkingDir/EncodedText/Dev2"
-    leaderboard_save_dir="./WorkingDir/EncodedText/Leaderboard"
-
+def main(
+    train_example_filepath:str,
+    dev1_example_filepath:str,
+    dev2_example_filepath:str,
+    leaderboard_example_filepath:str,
+    context_filepath:str,
+    bert_model_dir:str,
+    train_save_dir:str,
+    dev1_save_dir:str,
+    dev2_save_dir:str,
+    leaderboard_save_dir:str):
     encoder=ExampleEncoder(context_filepath,bert_model_dir)
-    #encoder.encode_save(train_example_filepath,train_save_dir)
+    encoder.encode_save(train_example_filepath,train_save_dir)
     encoder.encode_save(dev1_example_filepath,dev1_save_dir)
-    #encoder.encode_save(dev2_example_filepath,dev2_save_dir)
-    #encoder.encode_save(leaderboard_example_filepath,leaderboard_save_dir)
+    encoder.encode_save(dev2_example_filepath,dev2_save_dir)
+    encoder.encode_save(leaderboard_example_filepath,leaderboard_save_dir)
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser()
+
+    parser.add_argument("--train_example_filepath",type=str)
+    parser.add_argument("--dev1_example_filepath",type=str)
+    parser.add_argument("--dev2_example_filepath",type=str)
+    parser.add_argument("--leaderboard_example_filepath",type=str)
+    parser.add_argument("--context_filepath",type=str)
+    parser.add_argument("--bert_model_dir",type=str)
+    parser.add_argument("--train_save_dir",type=str)
+    parser.add_argument("--dev1_save_dir",type=str)
+    parser.add_argument("--dev2_save_dir",type=str)
+    parser.add_argument("--leaderboard_save_dir",type=str)
+
+    args=parser.parse_args()
+
+    main(
+        args.train_example_filepath,
+        args.dev1_example_filepath,
+        args.dev2_example_filepath,
+        args.leaderboard_example_filepath,
+        args.context_filepath,
+        args.bert_model_dir,
+        args.train_save_dir,
+        args.dev1_save_dir,
+        args.dev2_save_dir,
+        args.leaderboard_save_dir
+    )
