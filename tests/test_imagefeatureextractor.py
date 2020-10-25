f51f32a1601c013d7e2cd53b7a31e5903c6b5060
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-10-25 22:18:12 +0900

    更新

--- tests/test_imagefeatureextractor.py
+++ tests/test_imagefeatureextractor.py
@@ -11,14 +11,28 @@ logging.basicConfig(format=logging_fmt)
 
 def main(
     image_root_dir:str,
-    save_dir:str):
+    save_dir:str,
+    index_lower_bound:int,
+    index_upper_bound:int):
     extractor=ImageFeatureExtractor()
-    extractor.extract(image_root_dir,save_dir)
+    extractor.extract(
+        image_root_dir,
+        save_dir,
+        index_lower_bound=index_lower_bound,
+        index_upper_bound=index_upper_bound
+    )
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("--image_root_dir",type=str)
     parser.add_argument("--save_dir",type=str)
+    parser.add_argument("--index_lower_bound",type=int,default=-1)
+    parser.add_argument("--index_upper_bound",type=int,default=-1)
     args=parser.parse_args()
 
-    main(args.image_root_dir,args.save_dir)
+    main(
+        args.image_root_dir,
+        args.save_dir,
+        args.index_lower_bound,
+        args.index_upper_bound
+    )
