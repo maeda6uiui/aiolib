ab8240ccb73786e3b8b07669a3225a36b1ef3bc9
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-11-01 10:32:17 +0900

    テストコードを更新

--- tests/test_imagefeatureextractor.py
+++ tests/test_imagefeatureextractor.py
@@ -11,13 +11,15 @@ logging.basicConfig(format=logging_fmt)
 
 def main(
     image_root_dir:str,
-    save_dir:str,
+    boxes_save_dir:str,
+    features_save_dir:str,
     index_lower_bound:int,
     index_upper_bound:int):
     extractor=ImageFeatureExtractor()
     extractor.extract(
         image_root_dir,
-        save_dir,
+        boxes_save_dir,
+        features_save_dir,
         index_lower_bound=index_lower_bound,
         index_upper_bound=index_upper_bound
     )
@@ -25,14 +27,16 @@ def main(
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument("--image_root_dir",type=str)
-    parser.add_argument("--save_dir",type=str)
+    parser.add_argument("--boxes_save_dir",type=str)
+    parser.add_argument("--features_save_dir",type=str)
     parser.add_argument("--index_lower_bound",type=int,default=-1)
     parser.add_argument("--index_upper_bound",type=int,default=-1)
     args=parser.parse_args()
 
     main(
         args.image_root_dir,
-        args.save_dir,
+        args.boxes_save_dir,
+        args.features_save_dir,
         args.index_lower_bound,
         args.index_upper_bound
     )
--- tests/test_wikipediaimagefeatureextractor.py
+++ tests/test_wikipediaimagefeatureextractor.py
@@ -12,21 +12,24 @@ logging.basicConfig(format=logging_fmt)
 def main(
     article_list_filepath:str,
     image_root_dir:str,
-    save_dir:str):
+    boxes_save_dir:str,
+    features_save_dir:str):
     extractor=WikipediaImageFeatureExtractor(article_list_filepath)
-    extractor.extract(image_root_dir,save_dir)
+    extractor.extract(image_root_dir,boxes_save_dir,features_save_dir)
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
 
     parser.add_argument("--article_list_filepath",type=str)
     parser.add_argument("--image_root_dir",type=str)
-    parser.add_argument("--save_dir",type=str)
+    parser.add_argument("--boxes_save_dir",type=str)
+    parser.add_argument("--features_save_dir",type=str)
 
     args=parser.parse_args()
 
     main(
         args.article_list_filepath,
         args.image_root_dir,
-        args.save_dir
+        args.boxes_save_dir,
+        args.features_save_dir
     )
