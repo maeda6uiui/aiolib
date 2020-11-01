c4347114331fff703452848538e15267aa5ebdb0
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-11-01 11:26:41 +0900

    更新

--- tests/test_projector.py
+++ tests/test_projector.py
@@ -10,26 +10,37 @@ logging_fmt="%(asctime)s %(levelname)s: %(message)s"
 logging.basicConfig(format=logging_fmt)
 
 def main(
-    src_dim:int,
+    features_src_dim:int,
     dst_dim:int,
-    src_dir:str,
-    save_dir:str):
-    projector=Projector(src_dim,dst_dim)
-    projector.project_from_directory(src_dir,save_dir)
+    boxes_src_dir:str,
+    boxes_save_dir:str,
+    features_src_dir:str,
+    features_save_dir:str):
+    projector=Projector(features_src_dim,dst_dim)
+    projector.project_from_directory(
+        boxes_src_dir,
+        boxes_save_dir,
+        features_src_dir,
+        features_save_dir
+    )
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
 
-    parser.add_argument("--src_dim",type=int)
+    parser.add_argument("--features_src_dim",type=int)
     parser.add_argument("--dst_dim",type=int)
-    parser.add_argument("--src_dir",type=str)
-    parser.add_argument("--save_dir",type=str)
+    parser.add_argument("--boxes_src_dir",type=str)
+    parser.add_argument("--boxes_save_dir",type=str)
+    parser.add_argument("--features_src_dir",type=str)
+    parser.add_argument("--features_save_dir",type=str)
 
     args=parser.parse_args()
 
     main(
-        args.src_dim,
+        args.features_src_dim,
         args.dst_dim,
-        args.src_dir,
-        args.save_dir
+        args.boxes_src_dir,
+        args.boxes_save_dir,
+        args.features_src_dir,
+        args.features_save_dir
     )
