906ccf6ec7e8903022dfe20e3d83524058ea3b20
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-11-01 11:23:17 +0900

    更新

--- src/aiolib/util/image/projector.py
+++ src/aiolib/util/image/projector.py
@@ -24,31 +24,55 @@ class Projector(object):
     """
     特徴量ベクトルの射影を行う。
     """
-    def __init__(self,src_dim,dst_dim,seed:int=42,logger:logging.Logger=default_logger):
+    def __init__(
+        self,
+        features_src_dim:int,
+        dst_dim:int,
+        seed:int=42,
+        logger:logging.Logger=default_logger):
         set_seed(seed)
         
-        self.src_dim=src_dim
-        self.fc=torch.nn.Linear(src_dim,dst_dim).to(device)
+        self.fc_boxes=torch.nn.Linear(4,dst_dim).to(device)
+        self.fc_features=torch.nn.Linear(features_src_dim,dst_dim).to(device)
 
         self.logger=logger
 
-    def project(self,v:torch.Tensor)->torch.Tensor:
-        return self.fc(v)
+    def project_boxes(self,v:torch.Tensor)->torch.Tensor:
+        return self.fc_boxes(v)
+    def project_features(self,v:torch.Tensor)->torch.Tensor:
+        return self.fc_features(v)
 
-    def project_from_directory(self,src_dir:str,save_dir:str):
+    def project_from_directory(
+        self,
+        boxes_src_dir:str,
+        boxes_save_dir:str,
+        features_src_dir:str,
+        features_save_dir:str):
         logger=self.logger
 
-        os.makedirs(save_dir,exist_ok=True)
+        os.makedirs(boxes_save_dir,exist_ok=True)
+        os.makedirs(features_save_dir,exist_ok=True)
 
-        files=os.listdir(src_dir)
+        #Boxes
+        logger.info("RoI座標の射影を開始します。")
+
+        files=os.listdir(boxes_src_dir)
         for file in tqdm(files):
-            src_filepath=os.path.join(src_dir,file)
+            src_filepath=os.path.join(boxes_src_dir,file)
             src_vec=torch.load(src_filepath,map_location=device).to(device)
-            if src_vec.size(1)!=self.src_dim:
-                logger.error("入力ベクトルのサイズが不正です。 ファイル: {} サイズ: {}".format(file,src_vec.size()))
-                continue
+            dst_vec=self.fc_boxes(src_vec)
 
-            dst_vec=self.fc(src_vec)
+            save_filepath=os.path.join(boxes_save_dir,file)
+            torch.save(dst_vec,save_filepath)
 
-            save_filepath=os.path.join(save_dir,file)
+        #Features
+        logger.info("RoI特徴量の射影を開始します。")
+
+        files=os.listdir(features_src_dir)
+        for file in tqdm(files):
+            src_filepath=os.path.join(features_src_dir,file)
+            src_vec=torch.load(src_filepath,map_location=device).to(device)
+            dst_vec=self.fc_features(src_vec)
+
+            save_filepath=os.path.join(features_save_dir,file)
             torch.save(dst_vec,save_filepath)
