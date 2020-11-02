dadb1f7a8b85253c327cd882b7c3b3556ca58ff8
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-11-02 09:36:36 +0900

    更新

--- tests/test_fasterrcnnmodeler.py
+++ tests/test_fasterrcnnmodeler.py
@@ -13,9 +13,16 @@ def main(
     train_input_dir:str,
     dev_input_dir:str,
     bert_model_dir:str,
+    im_boxes_dir:str,
     im_features_dir:str,
     result_save_dir:str):
-    modeler=FasterRCNNModeler(train_input_dir,dev_input_dir,bert_model_dir,im_features_dir)
+    modeler=FasterRCNNModeler(
+        train_input_dir,
+        dev_input_dir,
+        bert_model_dir,
+        im_boxes_dir,
+        im_features_dir
+    )
     modeler.train_and_eval(result_save_dir=result_save_dir)
 
 if __name__=="__main__":
@@ -24,6 +31,7 @@ if __name__=="__main__":
     parser.add_argument("--train_input_dir",type=str)
     parser.add_argument("--dev_input_dir",type=str)
     parser.add_argument("--bert_model_dir",type=str)
+    parser.add_argument("--im_boxes_dir",type=str)
     parser.add_argument("--im_features_dir",type=str)
     parser.add_argument("--result_save_dir",type=str)
 
@@ -33,6 +41,7 @@ if __name__=="__main__":
         args.train_input_dir,
         args.dev_input_dir,
         args.bert_model_dir,
+        args.im_boxes_dir,
         args.im_features_dir,
         args.result_save_dir
     )
